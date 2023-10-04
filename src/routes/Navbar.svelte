bfd20d6cda63b443857a7ab984d75e42ea815d01
Mike Zeng <mzen17.jp@gmail.com>
2023-10-04 14:48:23 -0500

    Fixed a bug with github icon URL

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -2,6 +2,7 @@
     <img src="https://drive.google.com/uc?export=view&id=1-hNKf_ceozK7WtQybUeY9l-BiXfDdK1o" class="w-16 h-16" alt="WC3 Logo"/>
 
     <div class="flex items-center ml-auto">
-        <a href="https://github.com/West-Computing-Club"><img src="/github.svg" class="w-6 h-6 mx-2 opacity-50 transition-all hover:opacity-100" alt="WC3 Github"/></a>
+        <a href="https://github.com/West-Computing-Club">
+            <img src="https://myoctocat.com/assets/images/GitHub-logo.svg" class="w-6 h-6 mx-2 opacity-50 transition-all hover:opacity-100" alt="WC3 Github"/></a>
     </div>
 </nav>
